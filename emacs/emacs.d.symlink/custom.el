6a8077684a79087ae7590308ea3efeeaed78fb6a
Stuart Hacking <stuart.hacking@oracle.com>
2013-01-25 10:08:38 +0000

    Re-enabling slime options

--- emacs/emacs.d.symlink/custom.el
+++ emacs/emacs.d.symlink/custom.el
@@ -67,3 +67,11 @@
  '(visual-line-mode nil t)
  '(yow-file "~/.emacs.d/yow.lines"))
 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "gray95" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 106 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:foreground "LightSalmon" :weight extra-bold))))
+ '(hl-line ((t (:background "gray5")))))
--- emacs/emacs.d.symlink/init/init-cl.el
+++ emacs/emacs.d.symlink/init/init-cl.el
@@ -1,6 +1,6 @@
 ;;; init-cl.el --- Initialise options for Common^W Lisp programming
 ;;;
-;(require 'rainbow-delimiters)
+(require 'rainbow-delimiters)
 ;(require 'cldoc)
 (require 'slime)
 
@@ -10,20 +10,21 @@
 ;;  (setq-default frame-background-mode 'dark))
 
 ;; hook rainbow mode into any lisp mode
-;(dolist (hook '(lisp-mode-hook
-;                slime-repl-mode-hook
-;                emacs-lisp-mode-hook))
-;  (add-hook hook 'rainbow-delimiters-mode))
-
-;; hook cldoc into common lisp modes
 (dolist (hook '(lisp-mode-hook
-                slime-repl-mode-hook))
-  (add-hook hook 'slime-autodoc-mode))
+                slime-repl-mode-hook
+                emacs-lisp-mode-hook))
+  (add-hook hook 'rainbow-delimiters-mode))
+
+
 
 ;; Slime Setup
 (eval-after-load "slime"
   (progn
-    (slime-setup '(slime-fancy slime-banner))))
+    (slime-setup '(slime-fancy
+                   slime-scratch
+                   slime-banner
+                   slime-autodoc
+                   slime-highlight-edits))))
 
 ;(when (eq system-type 'darwin)
 ;  (setq common-lisp-hyperspec-root "file:~/dev/lisp/HyperSpec-7-0/HyperSpec/"))
