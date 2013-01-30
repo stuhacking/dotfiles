dbc830e92c41cf1df57297db80cca908434590a2
Stuart Hacking <stuart.hacking@oracle.com>
2013-01-30 11:00:42 +0000

    org-mode init file.

---
+++ emacs/emacs.d.symlink/init/init-org.el
@@ -0,0 +1,13 @@
+;;; init-cl.el --- Initialise options for Common^W Lisp programming
+;;;
+(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/lisp/")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/contrib/lisp/" t)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;; init-cl.el ends here
