a9800041559009943516a2e0eac5bbdfcab217a7
Stuart Hacking <stuhacking@gmail.com>
2013-02-23 11:53:23 +0000

    Make emacs read the correct exec-path

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -58,6 +58,17 @@
 ;; Bind to help key.
 (global-set-key [C-f1] #'describe-function-at-point)
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 ;; always use short y/n confirmations.
 (defalias 'yes-or-no-p 'y-or-n-p)
 
