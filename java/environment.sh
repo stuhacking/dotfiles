aedfeac59b7c5b6f4c6ec6b2fe29b9cc2ebd855b
Stuart Hacking <stuhacking@gmail.com>
2013-11-29 10:06:29 +0000

    Fix JDK Path for Linux

--- java/environment.sh
+++ java/environment.sh
@@ -1,6 +1,6 @@
 
 if [ "$(uname)" == "Linux" ]
 then
-    export JAVA_HOME=/opt/jdk-1.6/jre
-    export PATH=$PATH:/opt/jdk-1.6/bin
+    export JAVA_HOME=/opt/jdk-1.6
+    export PATH=$PATH:${JAVA_HOME}/bin
 fi
