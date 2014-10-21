2181e17fa9dff4a503197b16e5fbf825fa0456e8
Stuart Hacking <stuhacking@gmail.com>
2014-10-21 10:02:25 +0100

    Add cider-nrepl to user profile.

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[jonase/eastwood "0.1.0"]
                   [lein-try "0.4.1"]
                   [lein-exec "0.3.1"]
-                  [lein-script "0.0.3"]]}}
+                  [lein-script "0.0.3"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
