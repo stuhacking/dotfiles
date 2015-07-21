04e44a2a8f11b65527e68d37f8b7947377e4bf36
Stuart Hacking <smh@incubus.home>
2015-07-21 20:04:28 +0100

    Detect non-system git install

--- git/environment.sh
+++ git/environment.sh
@@ -1,6 +1,9 @@
 export GIT_CUSTOM_DIR=$DOTFILES/git
 
-function git-current-branch () {
-    git branch | grep '*' | cut -d ' ' -f 2
-}
+GIT_INSTALL_DIR=/usr/local/git
 
+if [ -d "$GIT_INSTALL_DIR" ]; then
+    export PATH=$GIT_INSTALL_DIR/bin:$PATH
+    . $GIT_INSTALL_DIR/contrib/completion/git-prompt.sh
+    . $GIT_INSTALL_DIR/contrib/completion/git-completion.bash
+fi
