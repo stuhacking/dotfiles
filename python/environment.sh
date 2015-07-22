f1cd28faa8d14812921eefb3bc8a252ebdff8ad9
Stuart Hacking <stuhacking@gmail.com>
2015-07-22 23:23:52 +0100

    Prefer Anaconda Python dist.

--- python/environment.sh
+++ python/environment.sh
@@ -1,3 +1,9 @@
-if [ "Darwin" == `uname -s` ]; then
-    export PATH=/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
+if [ -d '//anaconda' ]; then
+    export PATH=//anaconda/bin:$PATH
 fi
+
+#VIRTUALENV_WRAPPER_SH=/usr/local/bin/virtualenvwrapper.sh
+#if [ -f $VIRTUALENV_WRAPPER_SH ]; then
+#    source $VIRTUALENV_WRAPPER_SH
+#fi
+
