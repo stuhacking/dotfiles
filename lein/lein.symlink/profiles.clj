9f9c13746ade246a1bbdd5df80462212c72aeb70
Stuart Hacking <stuhacking@gmail.com>
2016-09-30 16:38:47 +0100

    Updates: Gitconfig, Lein Profiles, Words

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,6 +1,13 @@
-{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+;;; Leiningen Global User Profile.
+{:user {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
                   [jonase/eastwood "0.2.1"]
+                  [lein-ancient "0.6.10"]
+                  [lein-gorilla "0.4.0"]
+                  [lein-pprint "1.1.2"]
                   [lein-try "0.4.3"]
-                  [lein-exec "0.3.5"]
-                  [lein-script "0.0.4"]]}}
+                  [lein-exec "0.3.6"]
+                  ;; Large tools
+                  [nightlight/lein-nightlight "1.6.5"]
+                  ;; Various REPL display improvements.
+                  [venantius/ultra "0.5.1"]
+                  [walmartlabs/vizdeps "0.1.6"]]}}
