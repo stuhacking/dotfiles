fab5042052a6c4ce472f2f64b4f36f37bb166234
Stuart Hacking <stuhacking@gmail.com>
2020-01-21 16:01:33 +0000

    Greatly Simplify Emacs config

--- emacs/alias.sh
+++ emacs/alias.sh
@@ -1,9 +1,7 @@
+# System aliases for Emacs
 
-# On OS X, try to use the latest installed emacs version
-if [ 'Darwin' == `uname -s` ]; then
-    if [ -e /Applications/Emacs.app ]; then
-	alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw -q'
-	alias memacs='open -a /Applications/Emacs.app'
-    fi
+if [ "Darwin" == `uname` ]
+then
+    alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
 fi
 
